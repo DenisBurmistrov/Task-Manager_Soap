7f5868fac4a0357fbaf852c81438f1ec36394126
DenisBurmistrov <6y6yka1997@gmail.com>
2019-05-07 10:27:06 +0300

    added files

---
+++ src/main/java/ru/burmistrov/soapTM/configure/HttpSessionApplicationInitializer.java
@@ -0,0 +1,6 @@
+package ru.burmistrov.soapTM.configure;
+
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+public class HttpSessionApplicationInitializer extends AbstractHttpSessionApplicationInitializer {
+}
