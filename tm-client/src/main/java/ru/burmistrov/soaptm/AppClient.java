8779a948d7ab486acc961078ea7e247c023c1bf3
d.burmistrov <6y6yka1997@gmail.com>
2019-05-16 17:51:21 +0300

    started services

--- tm-client/src/main/java/ru/burmistrov/soaptm/AppClient.java
+++ tm-client/src/main/java/ru/burmistrov/soaptm/AppClient.java
@@ -1,16 +1,30 @@
 package ru.burmistrov.soaptm;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.burmistrov.soaptm.api.endpoint.Exception_Exception;
+import ru.burmistrov.soaptm.api.endpoint.IAuthEndpoint;
 import ru.burmistrov.soaptm.api.endpoint.IProjectEndpoint;
 import ru.burmistrov.soaptm.configuration.SpringConfiguration;
+import ru.burmistrov.soaptm.endpoint.AuthEndpointService;
+
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.handler.MessageContext;
+import java.util.List;
+import java.util.Map;
 
 public class AppClient {
 
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws Exception_Exception {
         ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfiguration.class);
-        IProjectEndpoint projectEndpoint = context.getBean(IProjectEndpoint.class);
-        System.out.println(projectEndpoint);
+        IAuthEndpoint authEndpoint = context.getBean(IAuthEndpoint.class);
+        authEndpoint.auth("admin", "admin");
+        Map<String, List<String>> map = (Map<String, List<String>>)
+                ((BindingProvider)authEndpoint).getResponseContext().get(MessageContext.HTTP_RESPONSE_HEADERS);
+        System.out.println(map.get("Set-Cookie"));
+
+
     }
 
 }
